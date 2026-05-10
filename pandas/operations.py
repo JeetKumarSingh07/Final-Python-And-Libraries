6cb077a32931be742657df2768a6957c633b21b7
Jeetsingh07 <Jeetsingh07@users.noreply.github.com>
2026-05-10 11:35:27 +0530

    commit

--- pandas/operations.py
+++ pandas/operations.py
@@ -2,4 +2,9 @@ import pandas as pd
 import numpy as np
 data=pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
 data['C'] = data['A'] + data['B']
-print(data)
+# print(data)
+# print(data.shape)
+# print(data.columns)
+# print(data.dtypes)
+# print(data.info())
+print(data.describe())
